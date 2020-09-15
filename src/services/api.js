68b4a8cf42038fd10e2faf53aa5709f35144ee00
Marcos Paulo <marcosp.toledo@hotmail.com>
2020-09-15 17:23:55 -0300

    Fixed api link

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "my-json-server.typicode.com/MarcosPCToledo/RocketShoes/blob/master/"
+    baseURL: "http://my-json-server.typicode.com/MarcosPCToledo/RocketShoes"
 });
 
 export default api;
