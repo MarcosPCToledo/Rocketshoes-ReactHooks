c93ed8eb19008482b74861c8f3b746ec105e903b
MarcosPCToledo <59852809+MarcosPCToledo@users.noreply.github.com>
2020-09-17 15:40:43 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://my-json-server.typicode.com/MarcosPCToledo/RocketShoes"
+    baseURL: "https://my-json-server.typicode.com/MarcosPCToledo/RocketShoes"
 });
 
 export default api;
